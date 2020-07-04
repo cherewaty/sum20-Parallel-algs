67247817d3d12c9f3676dcdda6e1d0971b961f48
Jeffrey Cherewaty <jeffrey@cherewaty.com>
2020-07-04 14:58:28 -0500

    Copy Bellman Ford from upstream

--- assignments/hw2/hw2.go
+++ assignments/hw2/hw2.go
@@ -17,9 +17,58 @@ func Dijkstra(s graph.Node, g graph.Graph) Shortest {
 // Note that this uses Shortest to make it easier for you,
 // but you can use another struct if that makes more sense
 // for the concurrency model you chose.
-func BellmanFord(s graph.Node, g graph.Graph) Shortest {
+func BellmanFord(u graph.Node, g graph.Graph) (path Shortest) {
 	// Your code goes here.
-	return newShortestFrom(s, g.Nodes())
+	if !g.Has(u) {
+		return Shortest{from: u}
+	}
+	var weight Weighting
+	if wg, ok := g.(graph.Weighter); ok {
+		weight = wg.Weight
+	} else {
+		weight = UniformCost(g)
+	}
+
+	nodes := g.Nodes()
+
+	path = newShortestFrom(u, nodes)
+	path.dist[path.indexOf[u.ID()]] = 0
+
+	for i := 1; i < len(nodes); i++ {
+		changed := false
+		for j, u := range nodes {
+			for _, v := range g.From(u) {
+				k := path.indexOf[v.ID()]
+				w, ok := weight(u, v)
+				if !ok {
+					panic("bellman-ford: unexpected invalid weight")
+				}
+				joint := path.dist[j] + w
+				if joint < path.dist[k] {
+					path.set(k, joint, j)
+					changed = true
+				}
+			}
+		}
+		if !changed {
+			break
+		}
+	}
+
+	for j, u := range nodes {
+		for _, v := range g.From(u) {
+			k := path.indexOf[v.ID()]
+			w, ok := weight(u, v)
+			if !ok {
+				panic("bellman-ford: unexpected invalid weight")
+			}
+			if path.dist[j]+w < path.dist[k] {
+				return path
+			}
+		}
+	}
+
+	return path
 }
 
 // DeltaStep ...
